cc8f08feffc10fd16d7a61c2e3f4ddf9e1dcd0ca
rex wang <zhuroujiamomo@gmail.com>
2022-02-28 13:00:02 +1300

    Complete MarketDataController.

--- CanWeFixIt/CanWeFixItApi/Controllers/MarketDataController.cs
+++ CanWeFixIt/CanWeFixItApi/Controllers/MarketDataController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CanWeFixItService;
 using Microsoft.AspNetCore.Mvc;
@@ -6,15 +7,32 @@ using Microsoft.AspNetCore.Mvc;
 namespace CanWeFixItApi.Controllers
 {
     [ApiController]
-    [Route("v2/marketdata")]
+    [Route("v1/marketdata")]
     public class MarketDataController : ControllerBase
     {
+        private readonly IDatabaseService _database;
+
+        public MarketDataController(IDatabaseService database)
+        {
+            _database = database;
+        }
+
         // GET
         public async Task<ActionResult<IEnumerable<MarketDataDto>>> Get()
         {
-            // TODO:
-
-            return NotFound();
+            var marketData =  await _database.MarketData();
+            var instruments = await _database.Instruments();
+            var result = from m in marketData
+                         join i in instruments on m.Sedol equals i.Sedol
+                         where m.Active = true
+                         select new MarketDataDto()
+                         {
+                             Id = m.Id,
+                             Active = m.Active,
+                             DataValue = m.DataValue,
+                             InstrumentId = i.Id
+                         };
+            return Ok(result);
         }
     }
 }
